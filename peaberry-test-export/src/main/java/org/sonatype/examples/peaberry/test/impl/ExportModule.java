60d33e8e3c64001b67e83e3c5ead63c19804b108
Conor Gallagher <conor.gallagher@elasticpath.com>
2015-07-06 16:18:28 +0100

    Adding injection of a Provider<SomeType> to test how Sisu handles it. Gracefully it turns out!

--- peaberry-test-export/src/main/java/org/sonatype/examples/peaberry/test/impl/ExportModule.java
+++ peaberry-test-export/src/main/java/org/sonatype/examples/peaberry/test/impl/ExportModule.java
@@ -5,20 +5,22 @@ import static org.ops4j.peaberry.util.TypeLiterals.export;
 
 import javax.inject.Named;
 
+import com.google.inject.AbstractModule;
+
 import org.ops4j.peaberry.Peaberry;
 import org.sonatype.examples.peaberry.test.Scramble;
-
-import com.google.inject.AbstractModule;
 
 @Named
 class ExportModule
     extends AbstractModule
 {
+
+
     @Override
     protected void configure()
     {
         install( Peaberry.osgiModule() );
-        bind( export( Scramble.class ) ).toProvider( service( new ScrambleImpl() ).export() );
+        bind(export(Scramble.class)).toProvider(service(ScrambleImpl.class).export());
         // ^ append .asEagerSingleton(); if you want this service exported immediately
     }
 }
--- peaberry-test-export/src/main/java/org/sonatype/examples/peaberry/test/impl/ScrambleImpl.java
+++ peaberry-test-export/src/main/java/org/sonatype/examples/peaberry/test/impl/ScrambleImpl.java
@@ -1,31 +1,27 @@
 package org.sonatype.examples.peaberry.test.impl;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.sonatype.examples.peaberry.test.Scramble;
 
 // basic text scrambler implementation
-class ScrambleImpl
-    implements Scramble
-{
-    public String process( final String message )
+@Named
+public class ScrambleImpl implements Scramble {
+
+
+	private final ScrambleDelegate scrambleDelegate;
+
+	@Inject
+	public ScrambleImpl(
+			ScrambleDelegate scrambleDelegate) {
+		this.scrambleDelegate = scrambleDelegate;
+	}
+
+	@Override
+	public String process( final String message )
     {
-        final List<Character> charList = new ArrayList<Character>();
-        for ( final char c : message.toCharArray() )
-        {
-            charList.add( new Character( c ) );
-        }
 
-        Collections.shuffle( charList );
-
-        final char[] mixedChars = new char[message.length()];
-        for ( int i = 0; i < mixedChars.length; i++ )
-        {
-            mixedChars[i] = charList.get( i ).charValue();
-        }
-
-        return new String( mixedChars );
-    }
+		return scrambleDelegate.process(message);
+	}
 }
